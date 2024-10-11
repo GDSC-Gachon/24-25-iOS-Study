04f6a747f781fbad81cc10e1443e387366eb0053
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-10-11 14:29:36 +0900

    🎉 :: Project Setting

---
+++ Spotify_Clone/Spotify_Clone/App/Spotify_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_CloneApp.swift
+//  Spotify_Clone
+//
+//  Created by 채리원 on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spotify_Clone/Spotify_Clone/App/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotify_Clone
+//
+//  Created by 채리원 on 10/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
