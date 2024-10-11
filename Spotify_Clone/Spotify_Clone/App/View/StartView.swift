1a5d5b4609ab3a3fc3e829554fecdd77f9034bd6
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-10-11 15:03:29 +0900

    ✨ :: StartView

---
+++ Spotify_Clone/Spotify_Clone/App/View/StartView.swift
@@ -0,0 +1,24 @@
+//
+//  StartView.swift
+//  Spotify_Clone
+//
+//  Created by 채리원 on 10/11/24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        ZStack {
+            Color(.black).edgesIgnoringSafeArea(.all)  // edgesIgnoringSafeArea(.all) 사용하여 전체 배경 색상 변경하기
+            
+            VStack {
+                Image("MainLogo")
+            }
+        }
+    }
+}
+
+#Preview {
+    StartView()
+}
