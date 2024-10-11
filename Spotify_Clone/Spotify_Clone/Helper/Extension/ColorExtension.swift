26aa3959fa40a58efac265a998fbecf69c1b01c8
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-10-11 14:55:11 +0900

    ✨ :: ColorExtension

---
+++ Spotify_Clone/Spotify_Clone/Helper/Extension/ColorExtension.swift
@@ -0,0 +1,43 @@
+//
+//  ColorExtension.swift
+//  Spotify_Clone
+//
+//  Created by 채리원 on 10/11/24.
+//
+
+import SwiftUI
+
+extension Color {
+    // Surface colors
+    static let surfacePrimary = Color(uiColor: UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1))  // Black
+    static let surfaceSecondary = Color(uiColor: UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1))  // Dark Gray
+    static let surfaceGreen = Color(uiColor: UIColor(red: 0/255, green: 199/255, blue: 0/255, alpha: 1))  // Green
+    static let surfaceGreenDark = Color(uiColor: UIColor(red: 0/255, green: 128/255, blue: 0/255, alpha: 1))  // Dark Green
+    static let surfaceGray = Color(uiColor: UIColor(red: 128/255, green: 128/255, blue: 128/255, alpha: 1))  // Gray
+    static let surfaceWhite = Color(uiColor: UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1))  // White
+
+    // Text colors
+    static let textPrimary = Color(uiColor: UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1))  // White
+    static let textSecondary = Color(uiColor: UIColor(red: 128/255, green: 128/255, blue: 128/255, alpha: 1))  // Gray
+    static let textTertiary = Color(uiColor: UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1))  // Black
+    static let textGreen = Color(uiColor: UIColor(red: 0/255, green: 199/255, blue: 0/255, alpha: 1))  // Green
+
+    // Icon colors
+    static let iconPrimary = Color(uiColor: UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1))  // White
+    static let iconSecondary = Color(uiColor: UIColor(red: 128/255, green: 128/255, blue: 128/255, alpha: 1))  // Gray
+    static let iconGreen = Color(uiColor: UIColor(red: 0/255, green: 199/255, blue: 0/255, alpha: 1))  // Green
+    static let iconBlack = Color(uiColor: UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1))  // Black
+
+    // Chips colors
+    static let chipPrimary = Color(uiColor: UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1))  // Black
+    static let chipSecondary = Color(uiColor: UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1))  // White
+    static let chipGreen = Color(uiColor: UIColor(red: 0/255, green: 199/255, blue: 0/255, alpha: 1))  // Green
+
+    // Special colors
+    static let specialPurple = Color(uiColor: UIColor(red: 128/255, green: 0/255, blue: 128/255, alpha: 1))  // Purple
+    static let specialOrange = Color(uiColor: UIColor(red: 255/255, green: 165/255, blue: 0/255, alpha: 1))  // Orange
+    static let specialBlue = Color(uiColor: UIColor(red: 0/255, green: 0/255, blue: 255/255, alpha: 1))  // Blue
+
+    // Avatar variable colors
+    static let avatarPurple = Color(uiColor: UIColor(red: 147/255, green: 112/255, blue: 219/255, alpha: 1))  // Light Purple
+}
