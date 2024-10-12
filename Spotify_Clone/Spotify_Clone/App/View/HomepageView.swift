439ab9bf2b6976b9d07a9c98332fc874944f13ec
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-10-12 13:28:43 +0900

    🍱 :: Add TabBar Image

---
+++ Spotify_Clone/Spotify_Clone/App/View/HomepageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomepageView.swift
+//  Spotify_Clone
+//
+//  Created by 채리원 on 10/11/24.
+//
+
+import SwiftUI
+
+struct HomepageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomepageView()
+}
