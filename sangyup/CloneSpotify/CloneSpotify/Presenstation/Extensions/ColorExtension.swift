2052c90819a2d686e97886f096b0e332be66023e
sangyup <ideallyup@naver.com>
2024-10-13 00:54:26 +0900

    :lipstick: :: ColorExtension
    Add ColorExtension

---
+++ sangyup/CloneSpotify/CloneSpotify/Presenstation/Extensions/ColorExtension.swift
@@ -0,0 +1,17 @@
+//
+//  ColorExtension.swift
+//  CloneSpotify
+//
+//  Created by 이상엽 on 10/8/24.
+//
+
+import SwiftUI
+
+extension Color {
+    /// #121212
+    static let black = Color(uiColor: UIColor(red: 18/255, green: 18/255, blue: 18/255, alpha: 1))
+    /// #313131
+    static let gray = Color(uiColor: UIColor(red: 49/255, green: 49/255, blue: 49/255, alpha: 1))
+    /// #1ED760
+    static let green = Color(uiColor: UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1))
+}
