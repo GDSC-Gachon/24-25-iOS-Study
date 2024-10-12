8e04b81cac77af94e767a9c004b4cb275b16cc4b
sangyup <ideallyup@naver.com>
2024-10-13 00:47:32 +0900

    :tada: :: begin a project
    create xcode project

---
+++ sangyup/CloneSpotify/CloneSpotify/Presenstation/Views/CloneSpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneSpotifyApp.swift
+//  CloneSpotify
+//
+//  Created by 이상엽 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CloneSpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sangyup/CloneSpotify/CloneSpotifyTests/CloneSpotifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloneSpotifyTests.swift
+//  CloneSpotifyTests
+//
+//  Created by 이상엽 on 10/8/24.
+//
+
+import Testing
+@testable import CloneSpotify
+
+struct CloneSpotifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
