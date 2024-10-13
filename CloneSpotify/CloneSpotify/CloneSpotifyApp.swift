3b4c35106f03e6ac7817d94c89e04f33afc8d276
juwonleee <juwonlee0509@gmail.com>
2024-10-14 08:34:57 +0900

    🎉Setting: Setting Xcode Project

---
+++ CloneSpotify/CloneSpotify/CloneSpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneSpotifyApp.swift
+//  CloneSpotify
+//
+//  Created by Lee Juwon on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CloneSpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloneSpotify/CloneSpotifyTests/CloneSpotifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloneSpotifyTests.swift
+//  CloneSpotifyTests
+//
+//  Created by Lee Juwon on 10/12/24.
+//
+
+import Testing
+@testable import CloneSpotify
+
+struct CloneSpotifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
