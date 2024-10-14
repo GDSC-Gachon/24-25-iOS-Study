c917102f382e6df281b60732fbe8968a46f20ed2
juwonleee <juwonlee0509@gmail.com>
2024-10-14 09:04:10 +0900

    🍱Asset: Add Color Asset

---
+++ CloneSpotify/Shared/ImageLoaderView.swift
@@ -0,0 +1,19 @@
+//
+//  ImageLoaderView.swift
+//  CloneSpotify
+//
+//  Created by Lee Juwon on 10/14/24.
+//
+
+import SwiftUI
+import SDWebImage
+
+struct ImageLoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+}
