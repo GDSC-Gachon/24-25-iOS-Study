53ec2991ba19a756b5e3476490f1e38986fd9465
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-10-14 13:49:23 +0900

    ✨ :: GridModel

---
+++ Spotify_Clone/Spotify_Clone/App/Model/HomeModel/GridModel.swift
@@ -0,0 +1,14 @@
+//
+//  GridModel.swift
+//  Spotify_Clone
+//
+//  Created by 채리원 on 10/14/24.
+//
+
+import Foundation
+
+struct GridItem: Identifiable {
+    let id = UUID() 
+    let title: String
+    let imageName: String
+}
--- Spotify_Clone/Spotify_Clone/App/ViewModel/HomeViewModel/GridViewModel.swift
+++ Spotify_Clone/Spotify_Clone/App/ViewModel/HomeViewModel/GridViewModel.swift
@@ -5,4 +5,29 @@
 //  Created by 채리원 on 10/14/24.
 //
 
-import Foundation
+import SwiftUI
+
+class GridViewModel: ObservableObject {
+    @Published var gridItems: [[GridItem]] = []
+    
+    init() {
+        gridloadItems()
+    }
+    
+    // 데이터를 불러오는 함수 (나중에 수정 필요)
+    func gridloadItems() {
+        gridItems = [
+            [GridItem(title: "OK Computer", imageName: "Grid_1"),
+             GridItem(title: "OK Computer", imageName: "Grid_1")],
+            
+            [GridItem(title: "OK Computer", imageName: "Grid_1"),
+             GridItem(title: "OK Computer", imageName: "Grid_1")],
+            
+            [GridItem(title: "OK Computer", imageName: "Grid_1"),
+             GridItem(title: "OK Computer", imageName: "Grid_1")],
+            
+            [GridItem(title: "OK Computer", imageName: "Grid_1"),
+             GridItem(title: "OK Computer", imageName: "Grid_1")]
+        ]
+    }
+}
