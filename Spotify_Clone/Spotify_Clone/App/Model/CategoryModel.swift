d34b32ab73cd1e0df545aef591029666bf76aefb
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-10-14 12:06:04 +0900

    ✨ :: CategoryModel

---
+++ Spotify_Clone/Spotify_Clone/App/Model/CategoryModel.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryModel.swift
+//  Spotify_Clone
+//
+//  Created by 채리원 on 10/14/24.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case All, Music, Podcasts
+}
